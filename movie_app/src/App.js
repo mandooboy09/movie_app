d17d6f2c6c14ac28d7a40c24ec6e4e4d1b28b22f
Taeyong Song <mandooboy09@gmail.com>
2019-05-16 20:54:15 +0900

    비동기 방식 추가

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,64 +4,33 @@ import './App.css';
 import Movie from "./Movie.js";
 
 class App extends React.Component {
-  state = {
-//    movies : [
-//      {
-//        title: "The Avengers : Assembled",
-//        poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F9955CF375BC73E4B11"
-//      },
-//      {
-//        title: "The Avengers : Age of Ultron",
-//        poster: "https://t1.daumcdn.net/cfile/tistory/2728D346553A278E1F"
-//      },
-//      {
-//        title: "The Avengers : Infinity War",
-//        poster: "http://mblogthumb3.phinf.naver.net/MjAxODAzMTdfMjg0/MDAxNTIxMjg3NzAzMTg1.mLEm2Mb0DqTK1i_rS7EiH_ah6itRtiT2Jfj5U8ghheQg.Ki0y0cVVDoKHmC7rIzSOMcTCYYRI50KNQplt1SybxMQg.JPEG.c106507/gallery-1521208645-infinity-war-poster.jpg?type=w2"
-//      },
-//      {
-//        title: "The Avengers : End Game",
-//        poster: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBVYDl8.img?h=387&w=270&m=6&q=60&o=f&l=f&x=210&y=157"
-//      }
-//    ]    
-  }
+  state = {}
   
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-          //...this.state.moives
-          movies:[
-          {
-            title: "The Avengers : Assembled",
-            poster: "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F9955CF375BC73E4B11"
-          },
-          {
-            title: "The Avengers : Age of Ultron",
-            poster: "https://t1.daumcdn.net/cfile/tistory/2728D346553A278E1F"
-          },
-          {
-            title: "The Avengers : Infinity War",
-            poster: "http://mblogthumb3.phinf.naver.net/MjAxODAzMTdfMjg0/MDAxNTIxMjg3NzAzMTg1.mLEm2Mb0DqTK1i_rS7EiH_ah6itRtiT2Jfj5U8ghheQg.Ki0y0cVVDoKHmC7rIzSOMcTCYYRI50KNQplt1SybxMQg.JPEG.c106507/gallery-1521208645-infinity-war-poster.jpg?type=w2"
-          },
-          {
-            title: "The Avengers : End Game",
-            poster: "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BBVYDl8.img?h=387&w=270&m=6&q=60&o=f&l=f&x=210&y=157"
-          },
-          {
-            title : "Doctor strange",
-            poster : "https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory&fname=http%3A%2F%2Fcfile25.uf.tistory.com%2Fimage%2F25521A3B580F92BB29A65B"
-          }
-        ]
-
-      })
-    }, 5000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
     })
 
     return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    console.log(movies)
+    this.setState({
+      movies
+    })  
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render(){
