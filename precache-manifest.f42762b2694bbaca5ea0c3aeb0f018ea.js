8838cd6019d91e9b670718b56339c753870d5459
mandooboy09 <mandooboy09@gmail.com>
2019-05-20 21:57:36 +0900

    Updates

---
+++ precache-manifest.f42762b2694bbaca5ea0c3aeb0f018ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52226cdc00ca8d78bc86bbda78f86a88",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "53c7183fcc178d84b8ea",
+    "url": "/movie_app/static/css/main.50336350.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/movie_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "53c7183fcc178d84b8ea",
+    "url": "/movie_app/static/js/main.dc036873.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/movie_app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
